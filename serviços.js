1be37cbc8dcfff2cc53b3f33deb31b260c15ce4c
vitorbite <vitorbitencourt20@gmail.com>
2025-10-05 02:05:59 -0300

    Atualizando e incrementando a seção de Serviços

---
+++ serviços.js
@@ -0,0 +1,13 @@
+let serviço;
+function abrirServiço(s) {
+  console.log(s);
+  serviço = document.getElementById(s);
+  serviço.style.display = "block";
+  serviço.scrollTo({top: 0})
+  document.body.style.overflowY = "hidden";
+}
+
+function fecharServiço() {
+  serviço.style.display = "none";
+  document.body.style.overflowY = "scroll";
+}
